86102f22bcdebe130e9f4e9d98f71ee6e2ce4b4a
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-08 13:27:43 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Starting point
+ * 
+ * Return : Always 0 (Success)
+ */
 int main(void)
 {
-	/**
-	 * main - Starting point
-	 *
-	 * Return : Always 0 (success)
-	 */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
