d12a728e573bdc36128ffc0e40c84085e8b469fd
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-09 11:42:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabets in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet  <= 'z'; alphabet++)
+	{
+		putchar(letter);
+
+		putchat(\n);
+	}
+	return (0);
+}
