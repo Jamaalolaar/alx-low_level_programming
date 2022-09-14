c3b9c844e4f35fbaf4967098a74629de5e589d40
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-14 10:09:33 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - function checks if a lowercase letter is present
+ *
+ * Return: postive (1), negative (0)
+ */
+
+int _islower(int c)
+{
+	if (c >= a && c <= z)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
