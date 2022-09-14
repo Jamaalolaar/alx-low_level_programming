7b07c1d22400267287e5e75aabc85454b4b1f220
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-14 09:56:09 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints the first 10 alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int n = 0;
+	char c;
+
+	while (n <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+			_putchar('\n');
+		}
+		n++;
+	}
+}
