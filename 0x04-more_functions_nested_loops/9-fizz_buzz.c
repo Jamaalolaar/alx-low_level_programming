e0d0d07b3cc0f91b85796093ab8944a9f8085cf7
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-16 18:40:48 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz test
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (((i % 3) = 0) && ((i % 5) != 0))
+		{
+			continue;
+			printf("Fizz");
+		}
+		else if (((i % 5) = 0) && ((i % 3) != 0))
+		{
+			continue;
+			printf("Buzz");
+		}
+		else if ((i % 15) = 0)
+		{
+			continue;
+			printf("FizzBuzz");
+		}
+		else
+			_putchar(i + '0');
+		_putchar(' ');
+	}
+}
