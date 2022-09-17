9f1dffdfd20665ee7404af42eb612305f72b441e
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-16 17:04:25 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,15 @@
 void print_most_numbers(void)
 {
 	int i;
+	
 	for (i = 0; i <= 9; i++)
 	{
 		if ((i != 2) || (i != 4))
 		{
 			_putchar(i + '0');
 		}
+		else
+			continue;
 	}
 	_putchar('\n');
 }
