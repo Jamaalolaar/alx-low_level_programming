2fe1172a46f22355494257a3eaf3d36148f3fe40
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-19 15:18:27 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,7 +15,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i]);
+		if (i != n-1)
+		{
+			_putchar(a[i] + '0');
+			_putchar(' ');
+		}
+		else
+			_putchar(a[i] + '0');
 	}
 	_putchar('\n');
 }
