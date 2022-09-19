06a627df12eb5caad64dd4be5cc1904f60d2fce2
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-19 09:22:43 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string followed by a new line.
+ * @str: argument of function
+ * Return: void.
+ */
+
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != 0; a++)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}
