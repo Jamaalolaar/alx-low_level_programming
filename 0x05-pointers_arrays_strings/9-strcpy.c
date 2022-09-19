c30047c290cc3fd8d7261ad6f50a634bf90a2a82
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-19 15:51:23 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,18 @@
  * to the buffer pointed to by dest.
  * @dest: argument of function.
  * @src: argument of function.
+ * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest, src);
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(desr + i) = '\0';
+	return (dest);
 }
