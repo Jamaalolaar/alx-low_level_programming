c8ebab0e343d6eb43db7150de6ec27f0115f494c
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-22 10:06:12 -0700

    _strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,4 +4,11 @@
 #include <string.h>
 
 /**
- *
+ * _strncat - concatenates two strings but with an 'n'
+ * number of bytes from the second input.
+ * @dest: first input
+ * @src: second input
+ * @n: limiting number of bytes
+ */
+
+
