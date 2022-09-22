a870d9c07f4fafd02b7af11690b1888d40533fca
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-22 14:06:32 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,7 +17,7 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int difference;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (s1[i] == s2[i])
 		{
@@ -27,5 +27,5 @@ int _strcmp(char *s1, char *s2)
 			break;
 		i++;
 	}
-	return (*s1 - *s2);
+	return (s1 - s2);
 }
