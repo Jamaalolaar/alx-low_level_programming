0ba75b72fb2548c9574dd99816e0a304ea0199d5
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-22 09:58:57 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,18 +11,18 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i = 0;
 	int len = 0;
 
-	while (*dest != '\0')
+	while (dest [i++])
 	{
-		dest++;
-	}
-
-	while (*src != '\0')
-	{
-		dest += src[len];
 		len++;
 	}
-	*dest += '\0';
+
+	while (src[i])
+	{
+		dest[len++] = src[i];
+		i++;
+	}
 	return (dest);
 }
