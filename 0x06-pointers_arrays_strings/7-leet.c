59fad8a37b4fcab2bf9b6ba33294b529bf4d6a3e
Jamaalolaar <jamaalolaar@gmail.com>
2022-09-22 17:48:39 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * leet - encodes a string to 1337
+ * @s: string to encode
+ * Return: address of str
+ */
+
+char *leet(char *str)
+{
+	int a, b;
+	char x[] = "aeotlAEOTL";
+	Char y[] = "4307143071";
+
+	for (a = 0; s[a]; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (x[b] == *(str + a))
+				*(str + a) = y[b];
+		}
+	}
+	return (str);
+}
