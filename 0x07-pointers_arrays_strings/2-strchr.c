aaa93cc91aa4b779bf8d900da13cfb2b0eb94540
Jamaalolaar <jamaalolaar>
2022-09-26 18:03:16 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in string
+ * @c: character to be searched
+ * @s: pointer pointing to the string
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		;
+	}
+
+	while (a >= 0)
+	{
+		if (c = *(s + a))
+		{
+			break;
+		}
+		a--;
+	}
+	if (c != *(s + a))
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (s);
+	}
+}
