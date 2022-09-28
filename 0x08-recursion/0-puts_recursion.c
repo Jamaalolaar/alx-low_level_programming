c69fa5e2ea43f057c67d3a992fe8b5a333e30731
Jamaalolaar <jamaalolaar>
2022-09-28 07:34:54 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,10 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 	
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 	{
-		_putchar(s[i]);
-		_puts_recursion(s[i + 1]);
+		_putchar(*(s + i));
+		_puts_recursion(*(s(i + 1)));
 	}
 	_putchar('\n');
 }
