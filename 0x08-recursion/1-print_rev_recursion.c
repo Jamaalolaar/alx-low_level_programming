134f47b4e7a5b9a0850bfcf174030712f9d6acfa
Jamaalolaar <jamaalolaar>
2022-09-28 16:50:59 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,32 +4,15 @@
 /**
  * _print_rev_recursion - prints a string in reverse with a new line.
  * @s: pointer to the string
- * stringlength - gets the size of the string
- * @s: pointer to the string
  * Return: void.
  */
 
-int stringlength(char *s)
-{
-	int i = 0;
 
-	for (; s[i] != '\0'; i++)
-	{
-		;
-	}
-	return (i);
-}
 void _print_rev_recursion(char *s)
 {
-	int n = stringlength(*s);
-
-	if (n == 0)
+	if (s != '\0')
 	{
-		_putchar(*(s + n));
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-
-	_putchar(*(s + n));
-	_print_rev_recursion(*(s + (n - 1)));
 }
