12d3917bec0bff38f405b41318da5361cc3a6d1b
Jamaalolaar <jamaalolaar>
2022-09-28 20:06:00 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+int test(char *a, char *b)
+{
+	if (*a == *b)
+	{
+		return (1);
+	}
+	else
+		return (0);
+	return test((a + 1), (b + 1));
+}
+
+int is_palindrome(char *s)
+{
+	char *a;
+	char *b;
+
+	a = s;
+	is_palindrome(s + 1);
+	b = s;
+	return (test(a, b));
+}
+	
+	
+	
