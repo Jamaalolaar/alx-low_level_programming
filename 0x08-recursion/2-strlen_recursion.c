043dd21232bc57e658245590341cf842541191cf
Jamaalolaar <jamaalolaar>
2022-09-28 17:17:13 -0700

    betty doc

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * strlen_recursion - returns the length of a string.
+ * _strlen_recursion - returns the length of a string.
  * @s: pointer to the string
  * Return: length of string.
  */
