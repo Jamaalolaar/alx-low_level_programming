9663515ae22612778f779cd0572356efc1756235
Jamaalolaar <jamaalolaar>
2022-09-30 16:59:51 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print all arguments in a function
+ * @argc: argument counter
+ * @argv: argument pointer.
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i <= 0; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
