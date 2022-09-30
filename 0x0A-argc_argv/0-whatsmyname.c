f1f4208c38036f74f7a6019780345cd0fb42fd7d
Jamaalolaar <jamaalolaar>
2022-09-30 10:00:50 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of function
+ * @argc: argument count
+ * @argv: argument pointer
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
+		
