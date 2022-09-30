c65e9cff2286c7e6aced6473e089359165f3f1b7
Jamaalolaar <jamaalolaar>
2022-09-30 16:44:20 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -9,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
