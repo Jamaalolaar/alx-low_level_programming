e41aa4afb296dab234bae8d7e4ae9bc3b231c75a
Jamaalolaar <jamaalolaar>
2022-10-06 09:49:48 -0700

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * malloc_checked - works like malloc
+ * @b: argument of function
+ * Return: pointer to memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(a);
+	if (a == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
+
