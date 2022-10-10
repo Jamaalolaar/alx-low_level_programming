83e3af311a64ffa850fe5e581bbfcd38605108c3
Jamaalolaar <jamaalolaar>
2022-10-10 13:53:49 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,6 +53,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog->name);
 		return (NULL);
 	}
+	dog->owner = malloc(sizeof(char) * (_length(owner) + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
