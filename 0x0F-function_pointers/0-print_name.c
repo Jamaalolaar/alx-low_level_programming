1754d554c10c178f8f78d8b93858d78b38d31925
Jamaalolaar <jamaalolaar>
2022-10-12 15:12:08 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to a function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
+
