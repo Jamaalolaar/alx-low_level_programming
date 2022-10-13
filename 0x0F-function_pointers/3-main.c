24914da857565c99754c2de5e389c1a909eb1239
Jamaalolaar <jamaalolaar>
2022-10-12 17:34:55 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs basic calculations
+ * @argc: argument count
+ * @argv: pointer to array of argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int input1, input2, answer;
+	char operator;
+	int (*f)(int,int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	input1 = atoi(argv[1]);
+	f = get_op_func(argv[2]);
+	input2 = atoi(argv[3]);
+
+	if (!f)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	answer = f(input1, input2);
+	printf("%d\n", answer);
+
+	return(0);
+}
