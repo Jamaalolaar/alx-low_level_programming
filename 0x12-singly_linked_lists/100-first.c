fbf03843f9208e0c09d283dab91b81fe286e7e56
Jamaalolaar <jamaalolaar@gmail.com>
2022-10-20 18:54:02 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void __attribute__((constructor)) tortue(void);
  */
 void tortue(void)
 {
-	printf("You're beat! and yet, you moust allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you moust allow,");
+	printf("\nI bore my house upon my back!\n");
 }
