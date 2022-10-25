f9858c49d44d3f563c652fa2f0d32ef6e7746969
Jamaalolaar <jamaalolaar@gmail.com>
2022-10-25 13:34:08 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,9 +10,10 @@ size_t print_listint(const listint_t *h)
 	const listint_t *ind = h;
 	size_t counter = 0;
 
-	for (ind != NULL; counter++;)
+	while (ind != NULL)
 	{
 		printf("%d\n", ind->n);
+		counter++;
 		ind = ind->next;
 	}
 	return (counter);
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -14,6 +15,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 /**
  * struct listp_s - singly linked list
  * @p: pointers to the nodes
