d00830845583ea6ec202529ace4997752658cc9f
Jamaalolaar <jamaalolaar@gmail.com>
2022-11-15 16:59:39 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - find the possible combinations of three different numbers
+ * Return: void
+ */
+
+int main(void)
+{
+	int hundreds = 0, tens = 1, units = 2;
+
+	while (hundreds <= 7)
+	{
+		while (tens <= 8)
+		{
+			while (units <= 9)
+			{
+				putchar(hundreds + '0');
+				putchar(tens + '0');
+				putchar(units + '0');
+
+				if (tens != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				units++;
+			}
+			tens++;
+			units = tens + 1;
+		}
+		hundreds++;
+		tens = hundreds + 1;
+	}
+	putchar('\n');
+	return (0);
+}
