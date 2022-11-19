5a980447eae8ee1d8f41e981ba67766e0a79551f
Jamaalolaar <jamaalolaar@gmail.com>
2022-11-19 09:44:41 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,7 +22,7 @@ int main(void)
 					putchar(hundreds + '0');
 					putchar(tens + '0');
 					putchar(units + '0');
-					if (hundreds <= 7)
+					if (hundreds <= 7 && tens <= 8)
 					{
 						putchar(',');
 						putchar(' ');
