4c0421f07e712de9f4dccd4e0745a4a7d478a97a
Jamaalolaar <jamaalolaar@gmail.com>
2022-11-20 17:12:00 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - prints the sum of all even numbers less than
+ * 4,000,000 in the fibonacci series.
+ * Return: void
+ */
 int main(void)
 {
 	long int a = 1, b = 1, sum, evensum = 0;
