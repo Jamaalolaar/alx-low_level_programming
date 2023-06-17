1bcd8f189216f0fe0e850795f2f92cd89da633e2
Jamaalolaar <jamaalolaar@gmail.com>
2023-06-16 19:25:45 -0700

    betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,9 +8,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-  void *ptr = malloc(b);
+void *ptr = malloc(b);
 
-  if (ptr == NULL)
-    exit(98);
-  return (p);
+if (ptr == NULL)
+	exit(98);
+
+return (p);
 }
