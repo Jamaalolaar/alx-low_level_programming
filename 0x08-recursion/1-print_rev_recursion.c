76c9134bf82bcfca37edf3c257c8a720e5800706
Jamaalolaar <jamaalolaar@gmail.com>
2023-06-30 09:31:42 -0700

    void _print_rev_recursion(char *s)

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,39 +2,14 @@
 #include <stdio.h>
 
 /**
+ * _print_rev_recursion - prints  string in reversee
+ * @s: string to be printed
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	}
-}
-*/
-
-int _getlen(char *s)
-{
-	int n = 0;
-	while (s[n] != '\0')
-	{
-		n++;
-	}
-	return(n);
-}
-
-
-void _print_rev_recursion(char *s)
-{
-	int n = _getlen(s);
-
-	if (n == 0)
-	{
-		_putchar(s[n]);
-		return;
-	}
-	else
-	{
-		_putchar(s[n]);
-		_print_rev_recursion(&(s[n-1]));
 	}
 }
