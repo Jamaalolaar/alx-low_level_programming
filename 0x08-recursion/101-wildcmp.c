5ea3a4211c77df65b771814dcbe1f4d529c2f912
Jamaalolaar <jamaalolaar@gmail.com>
2023-07-02 08:48:06 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * wildcmp - checks if two strings are idenical
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if positive, 0 if negative
+ */
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 != '\0' && *s2 != '\0')
@@ -7,12 +13,12 @@ int wildcmp(char *s1, char *s2)
 		if (*s1 != *s2)
 		{
 			if (*s2 == '*')
-				return (wildcmp (s1, (s2 + 1)));
+				return (wildcmp(s1, (s2 + 1)) || wildcmp((s1 + 1), s2));
 			else
 				return (0);
 		}
 		else if (*s1 == *s2)
-			return (wildcmp ((s1 + 1), (s2 + 1)));
+			return (wildcmp((s1 + 1), (s2 + 1)));
 	}
 	return (1);
 }
