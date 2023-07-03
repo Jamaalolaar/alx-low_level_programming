b2324190768fbe32d327a1cf2626c6ea2c012929
Jamaalolaar <jamaalolaar@gmail.com>
2023-07-03 06:02:51 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * size_t list_len(const list_t *h) - counts the elements in
+ * a linked list.
+ * @h: list
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
