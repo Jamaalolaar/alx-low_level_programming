a6be84878c0c3a2956e8c7bfc48cc8e6a5fe3d15
Jamaalolaar <jamaalolaar@gmail.com>
2023-07-04 08:21:08 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,11 +8,9 @@
  * @argv: array of pointers to argument
  * Return: 0 is successful, 1 otherwise
  */
-
 int main(int argc, char *argv[])
 {
 	int n = atoi(argv[1]), change = 0;
-
 
 	if (argc == 2)
 	{
@@ -50,53 +48,6 @@ int main(int argc, char *argv[])
 	{
 		printf("%s\n", "Error");
 		return (1);
-	}
-	(void)argc;
-	return (0);
-}
-/**
-{
-	int n = atoi(argv[1]), a, b, c, d;
-	int change;
-
-	if (argc > 2 || argc < 2)
-	{
-		printf("%s\n", "Error");
-		return (1);
-	}
-	else
-	{
-		if (n < 0)
-			printf("%d\n", 0);
-		else if (n == 1)
-			change = 1;
-		else if (n >= 2 && n < 5)
-		{
-			change = (n / 2) + (n % 2);
-			printf("%d\n", change);
-		}
-		else if (n >= 5 && n < 10)
-		{
-			change = (n / 5) + ((n % 5) / 2) + ((n % 5) % 2);
-			printf("%d\n", change);
-		}
-		else if (n >= 10 && n < 25)
-		{
-			a = ((n % 10) / 5);
-			b = (((n % 10) % 5)/ 2);
-			c = (((n % 10) % 5) % 2);
-			change = (n / 10) + a + b + c;
-			printf("%d\n", change);
-		}
-		else if (n >= 25)
-		{
-			a = ((n % 25) / 10);
-			b = (((n % 25) % 10)/ 5);
-			c = ((((n % 25) % 10) % 5) / 2);
-			d = (((n % 25) % 10) % 5) % 2);
-			change = (n / 25) + a + b + c  + d;
-			printf("%d\n", change);
-		}
 	}
 	(void)argc;
 	return (0);
