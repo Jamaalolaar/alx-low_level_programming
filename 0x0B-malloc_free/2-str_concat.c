ed6fb1e105e3c13d4b8b9b82c12b7de75422e76c
Jamaalolaar <jamaalolaar@gmail.com>
2023-07-13 07:10:54 -0700

    task-2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,9 +1,19 @@
 #include "main.h"
-
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to conctenated string
+ */
 char *str_concat(char *s1, char *s2)
 {
 	int len1, len2, i, sum_len;
 	char *array;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (len1 = 0; s1[len1] != 0; len1++)
 		len1++;
@@ -14,36 +24,13 @@ char *str_concat(char *s1, char *s2)
 	array = malloc((sizeof(char) * sum_len) + 1);
 	if (array == NULL)
 		return (NULL);
-	if (s1 == NULL && s2 != NULL)
+	for (i = 0; i < len1; i++)
 	{
-		s1 = "";
-
-		for (i = 0; i < len2; len2++)
-			array[i] = s2[i];
-		array[len2] = '\0';
+		array[i] = s1[i];
 	}
-	else if (s2 == NULL && s1 != NULL)
+	for (i = 0; i + len1 < sum_len; i++)
 	{
-		s2 = "";
-		for (i = 0; i < len1; len1++)
-			array[i] = s1[i];
-		array[len1] = '\0';
-	}
-	else if (s1 == NULL && s2 == NULL)
-	{
-		s1 = "";
-		s2 = "";
-	}
-	else
-	{
-		for (i = 0; i < len1; i++)
-		{
-			array[i] = s1[i];
-		}
-		for (i = 0; i + len1 < sum_len; i++)
-		{
-			array[i + len1] = s2[i];
-		}
+		array[i + len1] = s2[i];
 	}
 	return (array);
 }
