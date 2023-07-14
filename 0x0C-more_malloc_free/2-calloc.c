95fa16da1b7d4124a8e9df84fefbd9554504f786
Jamaalolaar <jamaalolaar@gmail.com>
2023-07-14 04:43:42 -0700

    task-2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < nmemb)
+	{
+		(char)ptr[i] = 0;
+	}
+	return (ptr);
+}
