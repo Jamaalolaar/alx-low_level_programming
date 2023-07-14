290b2d2af5c87cdd5a71ab33906b5be486187ef8
Jamaalolaar <jamaalolaar@gmail.com>
2023-07-14 05:25:43 -0700

    task-3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int *array_range(int min, int max)
+{
+	int *array;
+	int i, size = max - min, a = min;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * size);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++, a++)
+	{
+		array[i] = a;
+	}
+	array[size] = max;
+	return (array);
+}
